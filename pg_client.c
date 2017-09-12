b3ed79dc8f94dd967e0da62bc036fa21bc9bd624
Frank Theile <frank.theile@web.de>
2017-09-12 23:08:22 +0200

    Open/close DB and initial error checking

--- pg_client.c
+++ pg_client.c
@@ -1,7 +1,23 @@
 #include <libpq-fe.h>
 
+PGconn* conn;
+
 int main(int argc, char* argv[])
 {
-	printf("Hello World!\n");
-	return 0;
+	int ret = 0;
+	if (argc == 2) {
+		conn = PQconnectdb(argv[1]);
+		if (PQstatus(conn) == CONNECTION_OK) {
+			// ...
+		} else {
+			printf("Error PQstatus()\n");
+			ret = -2;
+		}
+		PQfinish(conn);
+	} else {
+		printf("Usage:\n");
+		printf("  %s <connection string>\n", argv[0]);
+		ret = -1;
+	}
+	return ret;
 }
