e126ff5d228ef4eac74621a0668d11918a61fe8a
Frank Theile <frank.theile@web.de>
2017-09-12 23:47:33 +0200

    Query and print some data

--- pg_client.c
+++ pg_client.c
@@ -1,16 +1,39 @@
 #include <libpq-fe.h>
 
-PGconn* conn;
 
 int main(int argc, char* argv[])
 {
 	int ret = 0;
 	if (argc == 2) {
+		PGconn* conn;
 		conn = PQconnectdb(argv[1]);
 		if (PQstatus(conn) == CONNECTION_OK) {
-			// ...
+			PGresult* res;
+			res = PQexec(conn, "SELECT ts, value FROM my_data ORDER BY ts");
+			if (PQresultStatus(res) == PGRES_TUPLES_OK) {
+				int rows = PQntuples(res);
+				int cols = PQnfields(res);
+				int r, c;
+				// Column names:
+				for (c=0; c<cols; c++) {
+					printf("%s ", PQfname(res, c));
+				}
+				printf("\n");
+
+				// Data:
+				for (r=0; r<rows; r++) {
+					for (c=0; c<cols; c++) {
+						printf("%s ", PQgetvalue(res, r, c));
+					}
+					printf("\n");
+				}
+				PQclear(res);
+			} else {
+				printf("Error PQexec()\n");
+				ret = -3;
+			}
 		} else {
-			printf("Error PQstatus()\n");
+			printf("Error PQconnectdb()\n");
 			ret = -2;
 		}
 		PQfinish(conn);
