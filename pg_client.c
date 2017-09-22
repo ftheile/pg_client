8b2ed45a00e35807d7b542f6769e8cd145f075d4
Frank Theile <ftheile@grundfos.com>
2017-09-22 13:35:39 +0200

    Pass value to be written on the command line.

--- pg_client.c
+++ pg_client.c
@@ -4,13 +4,14 @@
 #ifdef BINARY_TRANSFER
 #include <arpa/inet.h>
 #endif
+#include <stdlib.h>
 
 #define ARG_CNT 3
 
 int main(int argc, char* argv[])
 {
 	int ret = 0;
-	if (argc == 2) {
+	if (argc == 3) {
 		PGconn* conn;
 		conn = PQconnectdb(argv[1]);
 		if (PQstatus(conn) == CONNECTION_OK) {
@@ -18,19 +19,19 @@ int main(int argc, char* argv[])
 			ExecStatusType est;
 
 			// Create prepared statement
-			res = PQprepare(conn, "add_value", "INSERT INTO results_float (data_id, ts, value) VALUES ($1, $2, $3)", 0, NULL);
+			res = PQprepare(conn, "add_value", "INSERT INTO results_float (ppid, ts, value) VALUES ($1, $2, $3)", 0, NULL);
 			est = PQresultStatus(res); 
 			PQclear(res);
 			if (est == PGRES_COMMAND_OK) {
 				// Execute the prepared statement:
 				int data_id = 1;
 				char ts[32];
-				float value = 4.321;
 				time_t t = time(NULL);
 				strftime(ts, sizeof(ts), "%F %T", localtime(&t));
 
 #ifdef BINARY_TRANSFER
 				// nbo_...: Temporary variables in network-byte-order
+				float value = atof(argv[2]);
 				uint32_t nbo_value = htonl(*(uint32_t*)(&value));
 				uint32_t nbo_data_id = htonl(data_id);
 
@@ -38,12 +39,10 @@ int main(int argc, char* argv[])
 				const int paramLengths[ARG_CNT] = { sizeof(nbo_data_id), 0, sizeof(nbo_value) };
 				const int paramFormats[ARG_CNT] = { 1, 0, 1 };
 #else
-				char str_value[16];
 				char str_data_id[16];
-				sprintf(str_value, "%f", value);
 				sprintf(str_data_id, "%d", data_id);
 
-				const char* paramValues[ARG_CNT] = { str_data_id, ts, str_value };
+				const char* paramValues[ARG_CNT] = { str_data_id, ts, argv[2] };
 				#define paramFormats NULL
 				#define paramLengths NULL
 #endif
@@ -65,7 +64,7 @@ int main(int argc, char* argv[])
 		PQfinish(conn);
 	} else {
 		printf("Usage:\n");
-		printf("  %s <connection string>\n", argv[0]);
+		printf("  %s <connection string> <value>\n", argv[0]);
 		ret = -1;
 	}
 	return ret;
