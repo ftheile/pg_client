8ebd6d36f3209d0a0a72cce8c1168b15d6862b23
Frank Theile <ftheile@grundfos.com>
2017-11-09 15:45:32 +0100

    Move creation of prepared statements into seperate function.

--- pg_client.c
+++ pg_client.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #ifdef BINARY_TRANSFER
 	#if ((defined _WIN64) || (defined _WIN32))
@@ -14,6 +15,43 @@
 
 #define ARG_CNT 4
 
+/* Create prepared statements.
+ * Parameters:
+ *   $1: GENI destination address of pump (e.g. "35")
+ *   $2: Name of data ID (e.g. "eData_ManuelFlow")
+ *   $3: Timestamp in ISO 8601 format (e.g. "YYYY-MM-DD hh:mm:ss")
+ *   $4: Value (e.g. "3.14")
+ */
+static bool CreatePreparedStmts(PGconn* conn)
+{
+	bool ret = true;
+	int i;
+#define STMT_CNT 3
+	static const struct {
+		const char* name;
+		const char* query;
+	} prep[STMT_CNT] = {
+		// name,            query
+		{ "insert_float",   "INSERT INTO results_float   (ppid, ts, value) SELECT ppid, $3, $4 FROM get_pump_params WHERE dest_addr = $1 AND name = $2" },
+		{ "insert_integer", "INSERT INTO results_integer (ppid, ts, value) SELECT ppid, $3, $4 FROM get_pump_params WHERE dest_addr = $1 AND name = $2" },
+//		{ "insert_id",      "INSERT INTO results_id      (ppid, ts, value) SELECT ppid, $3, $4 FROM get_pump_params WHERE dest_addr = $1 AND name = $2" }
+		{ "insert_id",      "INSERT INTO results_id      (ppid, ts, value) SELECT ppid, $3, $4::int2[] FROM get_pump_params WHERE dest_addr = $1 AND name = $2" }
+	};
+
+	for (i = 0; i < STMT_CNT; i++) {
+		if (ret) {
+			PGresult* res = PQprepare(conn, prep[i].name, prep[i].query, 0, NULL);
+			if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+				fprintf(stderr, PQresultErrorMessage(res));
+				ret = false;
+			}
+			PQclear(res);
+		}
+	}
+
+	return ret;
+}
+
 int main(int argc, char* argv[])
 {
 	int ret = 0;
@@ -21,21 +59,11 @@ int main(int argc, char* argv[])
 		PGconn* conn;
 		conn = PQconnectdb(argv[1]);
 		if (PQstatus(conn) == CONNECTION_OK) {
-			PGresult* res;
-			ExecStatusType status;
+			if (CreatePreparedStmts(conn)) {
+				PGresult* res;
+				ExecStatusType status;
 
-			/* Create prepared statement.
-			 * Parameters:
-			 *   $1: GENI destination address of pump (e.g. "35")
-			 *   $2: Name of data ID (e.g. "eData_ManuelFlow")
-			 *   $3: Timestamp in ISO 8601 format (e.g. "YYYY-MM-DD hh:mm:ss")
-			 *   $4: Value (e.g. "3.14")
-			 */
-			res = PQprepare(conn, "insert_float", "INSERT INTO results_float (ppid, ts, value) SELECT ppid, $3, $4 FROM get_pump_params WHERE dest_addr = $1 AND name = $2", ARG_CNT, NULL);
-			status = PQresultStatus(res); 
-			PQclear(res);
-			if (status == PGRES_COMMAND_OK) {
-				// Execute the prepared statement:
+				// Execute a prepared statement:
 				char ts[32];
 				time_t t = time(NULL);
 				strftime(ts, sizeof(ts), "%F %T", localtime(&t));
@@ -78,14 +106,11 @@ int main(int argc, char* argv[])
 #endif
 				res = PQexecPrepared(conn, "insert_float", ARG_CNT, paramValues, paramLengths, paramFormats, 0);
 				status = PQresultStatus(res); 
-				PQclear(res);
 				if (status != PGRES_COMMAND_OK) {
-					printf(PQerrorMessage(conn));
+					fprintf(stderr, PQresultErrorMessage(res));
 					ret = -4;
 				}
-			} else {
-				printf(PQerrorMessage(conn));
-				ret = -3;
+				PQclear(res);
 			}
 		} else {
 			printf(PQerrorMessage(conn));
